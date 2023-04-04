9a88fd11a36c4026d64169b058d6dcd748f179bd
ORIENTSOFTWARE\hoan.tran <hoan.tran@orientsoftware.com>
2023-04-04 17:27:11 +0700

    change UI

---
+++ src/components/about.js
@@ -0,0 +1,71 @@
+import React from "react"
+
+const AboutComponent = () => {
+  return (
+    <div className='container p-5 flex flex-col'>
+      <div className='block w-full p-6 bg-white border border-gray-200 rounded-lg shadow space-y-10'>
+        <div>
+          <div className='font-extrabold text-xl text-gray-900'>About me</div>
+          <div className='text-base text-gray-500'>I'm a self-driven and self-motivated person with
+            well communication, quick learner and be
+            interested in researching new things and also a
+            passionate software engineer with roughly two - year experience in software development that
+            mainly focus on React JS.</div>
+        </div>
+
+        <div className='block'>
+          <div className='font-extrabold text-xl text-gray-900'>Contact</div>
+          <div className='grid grid-rows-2 grid-flow-col gap-4'>
+            <div>
+              <div className='text-base text-gray-500'>ADDRESS</div>
+              <div className='text-base text-gray-900'>Da Nang</div>
+            </div>
+            <div>
+              <div className='text-base text-gray-500'>PHONE</div>
+              <div className='text-base text-gray-900'>0393 999 682</div>
+            </div>
+            <div>
+              <div className='text-base text-gray-500'>BIRTHDAY</div>
+              <div className='text-base text-gray-900'>01/08/1997</div>
+            </div>
+            <div>
+              <div className='text-base text-gray-500'>EMAIL</div>
+              <div className='text-base text-gray-900'>anhhoan1897@gmail.com</div>
+            </div>
+          </div>
+        </div>
+        <div className='block'>
+          <div className='font-extrabold text-xl text-gray-900'>Skill</div>
+          <div className='grid grid-rows-3 grid-flow-col gap-1'>
+            <div>HTML, CSS</div>
+            <div>REACT JS</div>
+            <div>AWS SERVICES</div>
+          </div>
+
+        </div>
+        <div className='block'>
+          <div className='font-extrabold text-xl text-gray-900'>Certificate</div>
+          <div className='text-base xl-gray-500'>IELTS 6.</div>
+        </div>
+        <div className='block'>
+          <div className='font-extrabold text-xl text-gray-900'>Interest</div>
+          <div className='grid grid-rows-3 grid-flow-col gap-1'>
+            <div>FOOTBALL</div>
+            <div>VIDEO GAME</div>
+          </div>
+        </div>
+        <div className='block'>
+          <div className='font-extrabold text-xl text-gray-900'>EDUCATION</div>
+          <p className='font-bold'>2015-2020</p>
+          <p className='font-bold text-lg'>DA NANG UNIVERSITY OF SCIENCE & TECHNOLOGY</p>
+          <p>
+            - Degree of Engineer: Embedded System engineer
+            - Average mark: 2.55/4
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(AboutComponent);
---
+++ src/components/experience.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const ExperienceComponent = () => {
+  return (
+    <div className='container p-5 flex flex-col'>
+      <div className='block w-full p-6 bg-white border border-gray-200 rounded-lg shadow space-y-5'>
+
+        <div className='block  space-y-3'>
+          <p className='font-extrabold text-xl text-gray-900'>EXPERIENCE</p>
+          <div className='block'>
+            <p className='font-bold'>11/2020 - 02/2021</p>
+            <p className='font-semibold'>INTERNSHIP</p>
+            <p className='font-medium'>Orient Software Development</p>
+            <p>
+              <span>-Description:</span> internship program includes
+              trainings in GoLang, React JS, Docker, GraphQL
+            </p>
+          </div>
+          <div className='block'>
+            <p className='font-bold'>03/2021 - 03/2022</p>
+            <p className='font-semibold'>SAMURAI (JAPAN)</p>
+            <p className='font-medium'>Orient Software Development</p>
+            <p>
+              <span className='font-bold'>-Description:</span> a system to manage financial assets
+              in a blockchain environment. Building mobile responsive include fetching api using ReactJS library
+            </p>
+            <p>
+              <span className='font-bold'>-Tech</span>: React JS.
+            </p>
+            <span className='font-bold'>-Role:</span>
+            <div className='grid gap-1'>
+              <div>Front-end Developer</div>
+              <div>Receive tasks from leader and break them down
+                into micro duties to follow</div>
+              <div>Develop and maintain the user interface</div>
+            </div>
+          </div>
+          <div className='block'>
+            <p className='font-bold'>04/2022 - Present</p>
+            <p className='font-semibold'>INCLOUD (JAPAN)</p>
+            <p className='font-medium'>Orient Software Development</p>
+            <p>
+              <span className='font-bold'>-Description:</span>  an observation service for all of their
+              clients. Using serverless to handle request and
+              response.
+            </p>
+            <p>
+              <span className='font-bold'>-Tech:</span>: React JS, Python, AWS Services.
+            </p>
+
+            <span className='font-bold'>-Role:</span>
+            <div className='grid gap-1'>
+              <div>Learn and use python to code lambda function
+                for back-end</div>
+              <div>Use ReactJS to develop front-end.</div>
+              <div>Learn and use service in AWS</div>
+              <div>Manual testing for frontend and write test case for backend</div>
+              <div>Prepare document for customer</div>
+            </div >
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default React.memo(ExperienceComponent);
