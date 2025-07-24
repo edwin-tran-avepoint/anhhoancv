4a0e8ff80470aec993af2ae7555241ba16122aa4
Edwin.Tran <edwin.tran@avepoint.com>
2025-07-24 13:22:39 +0700

    Update CV

--- src/components/experience.js
+++ src/components/experience.js
@@ -35,7 +35,7 @@ const ExperienceComponent = () => {
             </div>
           </div>
           <div className='block'>
-            <p className='font-bold'>04/2022 - Present</p>
+            <p className='font-bold'>04/2022 - 08/2023</p>
             <p className='font-semibold'>INCLOUD (JAPAN)</p>
             <p className='font-medium'>Orient Software Development</p>
             <p>
@@ -57,7 +57,78 @@ const ExperienceComponent = () => {
               <div>Prepare document for customer</div>
             </div >
           </div>
+           <div className='block'>
+            <p className='font-bold'>08/2023 - 02/2024</p>
+            <p className='font-semibold'>SMART HOME</p>
+            <p className='font-medium'>Avepoint Inc </p>
+            <p>
+              <span className='font-bold'>-Description:</span> an admin system to manage user and tracking their daily routine, as well as their work.
+            </p>
+            <p>
+              <span className='font-bold'>-Tech:</span>: React TS, GITLAB, JIRA.
+            </p>
+
+            <span className='font-bold'>-Role:</span>
+            <div className='grid gap-1'>
+              <div>Use React TS to develop the front-end.</div>
+              <div>Manual testing for frontend.</div>
+            </div >
+          </div>
+          <div className='block'>
+            <p className='font-bold'>02/2023 - 10/2024</p>
+            <p className='font-semibold'>Institute for Human Resource Professionals</p>
+            <p className='font-medium'>Avepoint Inc </p>
+            <p>
+              <span className='font-bold'>-Description:</span> a community service, connect user in HR field in Singapore into a sharing community.
+            </p>
+            <p>
+              <span className='font-bold'>-Tech:</span>: React JS, GITLAB, JIRA.
+            </p>
+            <span className='font-bold'>-Role:</span>
+            <div className='grid gap-1'>
+              <div>Use React JS to develop the front-end. Responsible module: Community management</div>
+              <div>Instruct team members on best practices and code reviews.</div>
+              <div>Manual testing for frontend.</div>
+              <div>Analyze business requirements and translate them into technical specifications.</div>
+            </div >
+          </div>
+          <div className='block'>
+            <p className='font-bold'>10/2023 - 02/2025</p>
+            <p className='font-semibold'>Institute for Human Resource Professionals - JRCOE</p>
+            <p className='font-medium'>Avepoint Inc</p>
+            <p>
+              <span className='font-bold'>-Description:</span> Create survey service for HR professionals in Singapore, allowing them to manage and have vision for their career path
+            </p>
+            <p>
+              <span className='font-bold'>-Tech:</span>: React JS, GITLAB, JIRA.
+            </p>
+            <span className='font-bold'>-Role:</span>
+            <div className='grid gap-1'>
+              <div>Use React JS to develop the front-end. Module Survey</div>
+              <div>Instruct team members on best practices and code reviews.</div>
+              <div>Manual testing for frontend.</div>
+              <div>Analyze business requirements and translate them into technical specifications.</div>
+            </div >
+          </div>
+          <div className='block'>
+            <p className='font-bold'>12/2025 - Present</p>
+            <p className='font-semibold'>HRIS</p>
+            <p className='font-medium'>Avepoint Inc</p>
+            <p>
+              <span className='font-bold'>-Description:</span>HR assist system. Assist HR and manager manage employee, goals, request and task... And provide create request function to request absence, extra time... 
+            </p>
+            <p>
+              <span className='font-bold'>-Tech:</span>: React TS, GITLAB, JIRA, .Net.
+            </p>
+            <span className='font-bold'>-Role:</span>
+            <div className='grid gap-1'>
+              <div>Use React JS to develop the front-end. Module Request, Task, Role Management.</div>
+              <div>Manual testing and automation testing for frontend.</div>
+              <div>Analyze business requirements and translate them into technical specifications.</div>
+            </div >
+          </div>
         </div>
+        
       </div>
 
     </div>
